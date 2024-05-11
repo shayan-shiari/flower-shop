8cc75abbb526c49f3d40c4b2621e7f54f7fef1a7
shayan <shayanshiari@gmail.com>
2024-05-11 10:26:01 -0700

    home=page added

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
