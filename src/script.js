7e66197bfdd30f8ae3ccb679c89630a9a2a56bda
shayan shiari <shayanshiari@gmail.com>
2024-05-12 12:08:51 +0330

    bugs fixed

--- src/script.js
+++ src/script.js
@@ -6,4 +6,21 @@ const swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
